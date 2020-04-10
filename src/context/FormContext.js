1bd6bf545c5f6688b10e344d7c80aa16b1ca4e51
Gustavo Monjardim <gustavo.monjardim@liftbank.com.br>
2020-04-09 23:28:39 -0300

    feat: implementa o FormContext com formik

---
+++ src/context/FormContext.js
@@ -0,0 +1,56 @@
+import { useFormik } from 'formik';
+import propTypes from 'prop-types';
+import React, { createContext, useContext, useMemo } from 'react';
+
+const FormContext = createContext();
+
+const FormProvider = ({ initialValues, validationSchema, onSubmit, children }) => {
+  const { handleChange, handleBlur, values, touched, errors, handleSubmit, submitForm } = useFormik(
+    {
+      initialValues,
+      validationSchema,
+      onSubmit,
+      validateOnChange: true,
+      validateOnMount: true,
+    }
+  );
+
+  const defaultContext = useMemo(
+    () => ({
+      handleChange,
+      handleBlur,
+      values,
+      touched,
+      errors,
+      handleSubmit,
+      submitForm,
+    }),
+    [handleChange, handleBlur, values, touched, errors, handleSubmit, submitForm]
+  );
+
+  return <FormContext.Provider value={defaultContext}>{children}</FormContext.Provider>;
+};
+
+FormProvider.propTypes = {
+  initialValues: propTypes.shape().isRequired,
+  validationSchema: propTypes.shape(),
+  onSubmit: propTypes.func.isRequired,
+  children: propTypes.oneOfType([
+    propTypes.element.isRequired,
+    propTypes.arrayOf(propTypes.element.isRequired),
+  ]).isRequired,
+};
+
+FormProvider.defaultProps = {
+  validationSchema: null,
+};
+
+function useForm() {
+  const context = useContext(FormContext);
+  if (context === undefined) {
+    throw new Error(`useForm must be used within a FormProvider`);
+  }
+  return context;
+}
+
+export { FormProvider, useForm };
