d4e2a5ed5c48846f6064d931ef48a797d8af5cd3
Gustavo Monjardim <gustavo.monjardim@liftbank.com.br>
2020-04-09 22:53:56 -0300

    feat: implementantando componente CheckoutForm

---
+++ src/components/CheckoutForm/checkoutForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CheckoutForm = () => {
+  return (
+    <div className="w-full max-w-screen-lg h-64 flex flex-row flex-wrap mx-8 bg-white border border-solid border-blue-400">
+      <div className="w-full md:w-1/3 bg-primary h-full"></div>
+      <div className="w-full md:w-2/3 bg-white h-full"></div>
+    </div>
+  );
+};
+
+export default CheckoutForm;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: '#DE4B4B',
+        dark: '#3C3C3C',
+      },
+    },
+  },
   variants: {},
   plugins: [],
 };
